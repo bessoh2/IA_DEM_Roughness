dd2cb3eff7057becbd7d023027dadd899d5a6d97
jsnakata10 <65931466+jsnakata10@users.noreply.github.com>
2021-05-24 10:40:02 -0700

    Update tile_slicer.py

--- tile_slicer.py
+++ tile_slicer.py
@@ -1,7 +1,8 @@
 def tile_slicer(im, shape):
     """Takes image and divides it into multiple tiles with equal specified shape.
     Shape should be a tuple with the desired tile size. Returns as a dicctionary with 
-    tile locations as keys of data type tuple."""
+    tile locations as keys of data type tuple. Returns tiled image, preserved shapes for padded tiles,
+    and locations for each original tile for stitching."""
     import numpy as np
     
     if type(shape) != tuple:
@@ -24,6 +25,8 @@ def tile_slicer(im, shape):
     shape_iter = list(shape)
     
     tile_dict = {}
+    preserved_shapes = {}
+    saved_locs = {}
     for i in range(1,int(div[1])+1): # iterates with different tiles locations until the whole image is captured 
         for j in range(1,int(div[0])+1):
             location = tuple([i,j])
@@ -33,15 +36,25 @@ def tile_slicer(im, shape):
             vl = vh - shape_iter[0]
             hh = i * shape_iter[1]
             hl = hh - shape_iter[1]
-            #print(f'{vl}:{vh}')
-            #print(f'{hl}:{hh}')
+            # print(f'{vl}:{vh}')
+            # print(f'{hl}:{hh}')
+            # ensures indexing is within the bounds
+            if vh > list(im_shape)[0]: 
+                vh = list(im_shape)[0]
+            if hh > list(im_shape)[1]:
+                hh = list(im_shape)[1]
+            
             subset = im[vl:vh, hl:hh]
+            saved_locs[location] = [vl, vh, hl, hh]
+            # print(saved_locs[location])
             if np.shape(subset) == shape:
                 tile_dict[location] = subset
             else:
                 #print('Desired Shape', shape)
+                pres_shape = np.shape(subset) # preserves shape so that padding can be removed
                 new_subset = universal_padding(subset, shape, False, True, False, True) #pads the bottom
                 #print('Actual Shape', np.shape(new_subset))
                 tile_dict[location] = new_subset
+                preserved_shapes[location] = pres_shape
             
-    return tile_dict
+    return tile_dict, preserved_shapes, saved_locs, im_shape
