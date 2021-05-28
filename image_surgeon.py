4faa59ee0d44a01717d3b1a6fe7dca3e0b598285
jsnakata10 <65931466+jsnakata10@users.noreply.github.com>
2021-05-28 12:58:55 -0700

    Update image_surgeon.py
    When importing the image, use xarray.open_rasterio so that the data type will not be ndarray.

--- image_surgeon.py
+++ image_surgeon.py
@@ -41,6 +41,7 @@ def tile_slicer(im, shape):
     tile locations as keys of data type tuple. Returns tiled image, preserved shapes for padded tiles,
     and locations for each original tile for stitching."""
     import numpy as np
+    #import xarray as xr
     
     if type(shape) != tuple:
         print('The specified shape is not in tuple form.')
@@ -62,7 +63,6 @@ def tile_slicer(im, shape):
     shape_iter = list(shape)
     
     tile_dict = {}
-    preserved_shapes = {}
     saved_locs = {}
     for i in range(1,int(div[1])+1): # iterates with different tiles locations until the whole image is captured 
         for j in range(1,int(div[0])+1):
@@ -83,18 +83,9 @@ def tile_slicer(im, shape):
             
             subset = im[vl:vh, hl:hh]
             saved_locs[location] = [vl, vh, hl, hh]
-            # print(saved_locs[location])
-            if np.shape(subset) == shape:
-                tile_dict[location] = subset
-            else:
-                #print('Desired Shape', shape)
-                pres_shape = np.shape(subset) # preserves shape so that padding can be removed
-                new_subset = universal_padding(subset, shape, False, True, False, True) #pads the bottom
-                #print('Actual Shape', np.shape(new_subset))
-                tile_dict[location] = new_subset
-                preserved_shapes[location] = pres_shape
+            tile_dict[location] = subset
             
-    return tile_dict, preserved_shapes, saved_locs, im_shape
+    return tile_dict, saved_locs, im_shape
   
   
   
@@ -111,12 +102,10 @@ def pad_cutter(tiles, preserved_shapes):
   
   
   
-def tif_stitch(tiles, preserved_shapes, saved_locs, im_shape):
+def tif_stitch(tiles, saved_locs, im_shape):
     """Creates a background mask the size of the original image shape and uses it to stitch the tiles back together.
     """
     import numpy as np
-    
-    trimmed_tiles = pad_cutter(tiles, preserved_shapes)
     
     stitched_im = np.zeros(im_shape)
     for key in saved_locs.keys():
