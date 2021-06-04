1a2aa9e1931e7b5c74495daea6612291866ea4f7
jsnakata10 <65931466+jsnakata10@users.noreply.github.com>
2021-06-04 10:08:29 -0700

    Update image_surgeon.py

--- image_surgeon.py
+++ image_surgeon.py
@@ -1,40 +1,3 @@
-def universal_padding(tile, shape, top, bottom, left, right):
-    """Takes a subset from an image and pads specified edges with NaN values 
-    to fit a standard array size. Requires True/False inputs for each edge, 
-    as well as a desired array shape. The edges should only be adjacent, never opposite."""
-    import numpy as np
-    
-    vert = np.array([0,0])
-    horiz = np.array([0,0])
-    
-    tile_size = np.shape(tile) # Finds the shape of the tile
-    t_array = np.array(tile_size) # Converts the shape from a tuple into an array
-    
-    pad_width = np.array(shape) - t_array # Should be a positive value
-    
-    if pad_width.all() < 0:
-        print('The given tile is larger than the specified shape')
-        return
-    
-    if bottom==True:
-        vert[1] += pad_width[0]
-    elif top==True:
-        vert[0] += pad_width[0]
-    
-    if right==True:
-        horiz[1] += pad_width[1]
-    elif left==True:
-        horiz[0] += pad_width[1]
-            
-    vert = tuple(vert)
-    horiz = tuple(horiz)
-    padded_tile = np.pad(tile, (vert, horiz), 'constant', constant_values=np.nan)
-    
-    return padded_tile
-
-  
-  
-  
 def tile_slicer(im, shape):
     """Takes image and divides it into multiple tiles with equal specified shape.
     Shape should be a tuple with the desired tile size. Returns as a dicctionary with 
@@ -87,21 +50,8 @@ def tile_slicer(im, shape):
             
     return tile_dict, saved_locs, im_shape
   
-  
-  
-def pad_cutter(tiles, preserved_shapes):
-    """Takes tiles that have had analysis performed on them and cuts out the padding"""
-    for key in preserved_shapes.keys():
-        im = tiles[key]
-        #print(preserved_shapes[key])
-        trim = list(preserved_shapes[key])
-        trimd_im = im[0:trim[0],0:trim[1]]
-        tiles[key] = trimd_im
-        
-    return tiles
-  
-  
-  
+
+    
 def tif_stitch(tiles, saved_locs, im_shape):
     """Creates a background mask the size of the original image shape and uses it to stitch the tiles back together.
     """
